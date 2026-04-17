696945d4651462fb5903c8385a77ae7a14beccfe
cyrushumphries <262752813+cyrushumphries@users.noreply.github.com>
2026-04-17 23:43:59 +0200

    UUID added

--- gui/musicvfx/graph/port_connection.py
+++ gui/musicvfx/graph/port_connection.py
@@ -1,7 +1,11 @@
+import uuid
+
 class PortConnection:
     """
     Represents a connection between two ports (edge of graph).
+    Ports are referenced by their IDs
     """
-    def __init__(self, src_port, dst_port):
-        self.src = src_port
-        self.dst = dst_port
+    def __init__(self, src_port_id, dst_port_id):
+        self.id_ = f"PortConnection_{uuid.uuid4().hex}"
+        self.src = src_port_id
+        self.dst = dst_port_id
