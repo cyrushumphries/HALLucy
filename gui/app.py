b846082fae5bf131725e3181158e1a265811f313
cyrushumphries <262752813+cyrushumphries@users.noreply.github.com>
2026-04-20 16:10:41 +0200

    Moved viewport creation here

--- gui/app.py
+++ gui/app.py
@@ -2,7 +2,17 @@ import dearpygui.dearpygui as dpg
 from musicvfx.main_window import setup_main_window
 
 def main():
+    dpg.create_context()
+    # dpg doens't report correct viewport sizes when starting maximized or calling 
+    # so we need to create a viewport size (taking into account the border size guesstimated values)
+    # will be corrected by resize callback in setup_main_window()
+
+    dpg.create_viewport(title="MusicVFX", width=1875, height=1000) 
+    dpg.setup_dearpygui()
+    dpg.show_viewport(maximized=True)
+    
     setup_main_window()
+
     dpg.start_dearpygui()
     dpg.destroy_context()
 
