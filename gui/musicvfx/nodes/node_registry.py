e1c4eb5df139246af6ddd1b97d3e26f499ddd05c
cyrushumphries <262752813+cyrushumphries@users.noreply.github.com>
2026-04-22 22:15:08 +0200

    Automatic node class register to track all node classes.

---
+++ gui/musicvfx/nodes/node_registry.py
@@ -0,0 +1,22 @@
+import pkgutil
+import importlib
+import inspect
+
+from musicvfx.graph.node import Node
+
+class NodeRegistry:
+    """ Automatic register of all nodes defined under musicvfx.nodes. """
+    def __init__(self):
+        self.node_classes = {}  # name -> {class -> category} dictionnary easy iteratable in the node_explorer
+
+    def discover(self, package="musicvfx.nodes"):
+        """
+        Automatically discover and import all modules under the musicvfx.nodes.
+        These nodes are all subclasses of musicvfx.graph.node.
+        """
+        pkg = importlib.import_module(package)
+        for module_info in pkgutil.walk_packages(pkg.__path__, pkg.__name__ + "."):
+            module = importlib.import_module(module_info.name)
+            for _, obj in inspect.getmembers(module, inspect.isclass):
+                if issubclass(obj, Node) and obj is not Node:
+                    self.node_classes[obj.NAME] = {"cls":obj, "category":obj.CATEGORY}
