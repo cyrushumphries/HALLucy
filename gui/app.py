ebfc5311fd32904d91e453b2ea6823de08f64400
cyrushumphries <262752813+cyrushumphries@users.noreply.github.com>
2026-05-30 22:52:25 +0200

    Screen scaling factor influences font scale

--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,5 @@
 import dearpygui.dearpygui as dpg
+import musicvfx.globals as globals
 from musicvfx.main_window import setup_main_window
 
 def main():
@@ -6,7 +7,7 @@ def main():
     # dpg doens't report correct viewport sizes when starting maximized or calling 
     # so we need to create a viewport size (taking into account the border size guesstimated values)
     # will be corrected by resize callback in setup_main_window()
-
+    dpg.set_global_font_scale(globals.scale_factor) # Consider the scale factor (important on 4K screens)
     dpg.create_viewport(title="MusicVFX", width=1875, height=1000) 
     dpg.setup_dearpygui()
     dpg.show_viewport(maximized=True)
