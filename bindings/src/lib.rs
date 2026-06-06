4fe698bedd2f2a35db7404c4bc7ddc3796cccfb0
cyrushumphries <262752813+cyrushumphries@users.noreply.github.com>
2026-06-06 22:15:07 +0200

    Updated binding layer with first method, to add a node to the graoh

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -5,3 +5,16 @@ use hallucy_engine::*;
 pub struct RustEngine {
     graph: EngineGraph,
 }
+
+#[pymethods]
+impl RustEngine {
+    #[new]
+    fn new() -> Self {
+        Self { graph: EngineGraph::new() }
+    }
+
+    fn add_node_by_name(py_graph: &mut EngineGraph, id: String, category: String, name: String, ports: Vec<PortDescriptor>) {
+        py_graph.add_node_by_name(id, category, name, ports);
+    }
+}
+
