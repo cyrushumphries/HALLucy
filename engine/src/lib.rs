3765469afe107d2ef39a8956b8dad51f4f30689c
cyrushumphries <262752813+cyrushumphries@users.noreply.github.com>
2026-06-06 22:13:06 +0200

    Updated mod and lib files

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,4 +1,9 @@
 pub mod engine_graph;
 pub mod buffer;
+pub mod node_trait;
+pub mod nodes;
 
 pub use engine_graph::EngineGraph;
+pub use node_trait::EngineNode;
+pub use node_trait::NodePortInfo;
+pub use node_trait::PortDescriptor;
